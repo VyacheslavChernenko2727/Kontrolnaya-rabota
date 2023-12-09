07cc809e1c7117b04e09da4f10664416e10b8eda
Vyacheslav <slavik-chernenko@yandex.ru>
2023-12-09 15:36:49 +0300

    Creat metod 2

--- Program.cs
+++ Program.cs
@@ -20,3 +20,25 @@ void StringLengthLessThanThree(string[] array1, string[] array2)
         }
     }
 }
+void StringLengthLessThanThree(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+StringLengthLessThanThree(array1, array2);
+PrintArray(array2);
