5eb6c3f3a224e63d01c271d6841154672c5e25e0
Vyacheslav <slavik-chernenko@yandex.ru>
2023-12-09 15:19:13 +0300

    Creat two array

--- Program.cs
+++ Program.cs
@@ -6,3 +6,5 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] array2 = new string[array1.Length];
