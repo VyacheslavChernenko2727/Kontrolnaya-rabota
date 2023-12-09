368ec57f5826a0a8fa635a12db0e1b8110478e19
Vyacheslav <slavik-chernenko@yandex.ru>
2023-12-09 15:17:46 +0300

    Add the task

--- Program.cs
+++ Program.cs
@@ -6,27 +6,3 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
-string[] array2 = new string[array1.Length];
-void StringLengthLessThanThree(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-StringLengthLessThanThree(array1, array2);
-PrintArray(array2);
