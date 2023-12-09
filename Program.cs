85fdce5097310c44033cf70d1affb500462b4169
Vyacheslav <slavik-chernenko@yandex.ru>
2023-12-09 15:19:50 +0300

    Creat metod 1

--- Program.cs
+++ Program.cs
@@ -8,3 +8,15 @@
 
 string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
 string[] array2 = new string[array1.Length];
+void StringLengthLessThanThree(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
